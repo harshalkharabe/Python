96cb3ebf41ac4c849000ad9e4353a2f797ffb2f0
Harshkharabe1 <harshal.kharabe@datamatter.tech>
2024-09-30 12:02:48 +0530

    Bitwise operator

--- OPERATORS/bitwise.py
+++ OPERATORS/bitwise.py
@@ -33,3 +33,74 @@ Syntax: opr<<n"""
 a = 0b1010
 print("Left shift in binary : ",bin(a<<2))
 print("Left shift in decimal : ",a<<2)
+
+"""Logic Gates
+A logic gate is a device that performs a Boolean function, a logical
+operation performed on one or more binary inputs that produces a single
+binary output.
+A logic gate is a device that acts as a building block for digital circuits. They
+perform basic logical functions that are fundamental to digital circuits.
+These operations are done using bitwise operators
+1. Bitwise & (AND) operator
+2. Bitwise | (OR) operator
+3. Bitwise ^ (XOR) operator
+4. Bitwise ~ (NOT) operator"""
+
+"""Bitwise (&) and Operator
+This operator is used for applying and gate.
+Truth table of (&amp;) operator
+Opr1    Opr2    Opr1&amp;Opr2
+1       1       1
+1       0       0
+0       1       0
+0       0       0"""
+
+a = 10      #1010
+b = 120  #1111000
+            #1000
+print(bin(a),bin(b))
+print("Value after AND (a&b) : ",a&b,bin(a&b))
+
+"""Bitwise (|) or operator
+This operator is used to apply or gate
+Truth table of bitwise | operator
+Opr1    Opr2    Opr1 | Opr2
+1       1       1
+1       0       1
+0       1       1
+0       0       0"""
+
+
+a = 10      #1010
+b = 120  #1111000
+         #1111010
+print(bin(a),bin(b))
+print("Value after OR (a|b) : ",a|b,bin(a|b))
+
+"""Bitwise ^ (XOR) operator
+This operator is used for applying XOR gate
+Truth table of XOR gate
+Opr1    Opr2   Opr1^Opr2
+1       1       0
+1       0       1
+0       1       1
+0       0       0"""
+
+
+a = 10      #1010
+b = 120  #1111000
+         #1110010
+print(bin(a),bin(b))
+print("Value after XOR (a^b) : ",a^b,bin(a^b))
+
+
+"""Bitwise not (~) operator
+Truth table of bitwise not (~) operator is
+Opr1    ~Opr1
+0       1
+1       0"""
+
+b = 120  #1111000
+         #0000111
+print(bin(b))
+print("Value after NOT (~b) : ",~b,bin(~b))
