95f8d6c033e7229438c3890cdeacf191eccebaf1
Harshkharabe1 <harshal.kharabe@datamatter.tech>
2024-10-01 11:07:16 +0530

    if statement

---
+++ CONTROL_STATEMENT/IF/if.py
@@ -0,0 +1,26 @@
+"""simple if
+if without else is called simple if
+Syntax:
+if condition:
+    statement-1
+    statement-2
+    statement-3
+if condition is True, python execute
+statement-1,statement-2 and
+statement-3
+if condition is False, python execute
+statement-3
+
+Note: empty blocks are not allowed in python. A block must have at least
+one statement."""
+
+if 10>2:
+    print("Harshal Kharabe")
+
+a = int(input("Enter number : "))
+if a==10:
+    print("Number is ",a)
+
+name = input("Enter name : ")
+if "omg"==name:
+    print("Equal name")
