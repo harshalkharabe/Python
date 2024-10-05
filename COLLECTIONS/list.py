6a7648de1b323c832cdb9dc73275b8e816af5f1b
Harshkharabe1 <harshal.kharabe@datamatter.tech>
2024-10-05 12:50:27 +0530

    list collection

---
+++ COLLECTIONS/list.py
@@ -0,0 +1,57 @@
+"""List data type
+Lists are mutable sequences, typically used to store collections of
+homogeneous items or heterogeneous items.
+How to create list?
+list can be represented in different ways
+1. Using empty square brackets to denote the empty list []
+2. Within square brackets more than one item is defined by separating
+with ,
+[item,item,item,item], [item]
+3. Using list comprehension
+4. Using list function (type conversion)
+a. list()
+b. list(iterable)
+
+Note: all collection types are called iterables, which can be iterated"""
+
+l1 = []
+print(l1,type(l1))
+l2 = [12,34,23,14]
+l3 = [1.23,3.44,23.12,11.4]
+l4 = [12,"python",False,14.90]
+print(l2,l3,l4,sep='\n')
+
+"""list() function
+This function is used to convert other iterables into list
+Syntax-1: list() empty list
+Syntax-2: list(iterable)  the construct list using existing iterable(OR)
+convert other iterables into list type"""
+
+l1 = list()
+print(l1,type(l1))
+l2 = list([12,9,34,66])
+# l3 = list(10)
+l4 = list("PYTHON")
+l5 = list(range(10,101,10))
+l6 = list(l2)
+
+print(l2,l4,l5,l6,sep='\n')
+
+"""How to read data or content from list or sequences?
+1. Using index
+2. Using slicing
+3. Using for loop
+4. Using iterator
+5. Using enumerate"""
+
+l1 = [10,20,30,40,50]
+
+print("Forward indexing")
+for i in range(0,5):
+    print(l1[i],end=' ')
+
+print()
+
+print("Backward indexing")
+for i in range(-5,0,1):
+    print(l1[i],end=' ')
