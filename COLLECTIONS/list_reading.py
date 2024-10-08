ccd6a319a22fcb8a540c9a22755d621e6a1caf8f
Harshkharabe1 <harshal.kharabe@datamatter.tech>
2024-10-09 00:34:16 +0530

    iterator and enumarate

--- COLLECTIONS/list_reading.py
+++ COLLECTIONS/list_reading.py
@@ -22,3 +22,34 @@ s2 = slice(-1,-12,-4)
 list1 = list(range(12,34,2))
 
 print(list1[s2])
+
+list1 = list(range(21,211,21))
+print(list1)
+
+list2 = list1[::-1]
+print(list2)
+
+list3 = list1[-8:5:]
+print(list3)
+
+list1 = [1,23,3]
+list1[len(list1):len(list1)]=[2]
+print(list1) 
+
+l1 = iter(list1)
+# print(next(l1))
+print(next(l1))
+print(next(l1))
+print(next(l1))
+print(next(l1))
+
+l1 = [1,2,3,4,5]
+b = iter(l1)
+for i in b:
+    print(i)
+
+list1 = [12,45,23,89,32]
+a = enumerate(list1)
+
+val = next(a)
+print(val,type(val))
