96891a2550c90a1184616411b6419f3ec75c4f23
Harshkharabe1 <harshal.kharabe@datamatter.tech>
2024-10-20 13:17:48 +0530

    tuple basic

--- COLLECTIONS/tuple/questions.py
+++ COLLECTIONS/tuple/questions.py
@@ -7,3 +7,11 @@ mi = l1[:k]
 mx = l1[-k:]
 t2 = tuple(mi+mx)
 print(t2)
+
+#python program to find cudes of given list values
+#INPUT = [9,5,2,6,8]
+#INPUT = [(9,729),(5,125),(2,8),(6,),(8,)]
+
+l1 = [9,5,2,6,8]
+l2 = [(num,num**3) for num in l1]
+print(l2)
