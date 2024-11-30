e55da944ef5fad9a27b8c1c359a31bd6df9a531f
Harshkharabe1 <harshal.kharabe@datamatter.tech>
2024-11-30 14:30:11 +0530

    Exception handling

--- EXCEPTION_HANDLING/exception.py
+++ EXCEPTION_HANDLING/exception.py
@@ -78,12 +78,162 @@
 
 
 # Error with empty except
-while True:
-    try:
-        n1 = int(input("Enter value :"))
-        n2 = int(input("Enter value :"))
-        n3 = n1/n2
-        print(f"{n1}/{n2} = {n3}")
-        break
-    except:
-        print(f"Plz.. enter valid integer or don't enter zero")
+# while True:
+#     try:
+#         n1 = int(input("Enter value :"))
+#         n2 = int(input("Enter value :"))
+#         n3 = n1/n2
+#         print(f"{n1}/{n2} = {n3}")
+#         break
+#     except:
+#         print(f"Plz.. enter valid integer or don't enter zero")
+
+# from bcrypt import gensalt, hashpw, checkpw
+
+# def hash_password(plain_password: str) -> str:
+#     """Hash the plain password using bcrypt."""
+#     salt = gensalt()
+#     hashed_password = hashpw(plain_password.encode('utf-8'), salt)
+#     return hashed_password.decode('utf-8')
+
+# def verify_password(plain_password: str, hashed_password: str) -> bool:
+#     print("Password verification :",plain_password.encode('utf-8'),hashed_password.encode('utf-8'))
+#     return checkpw(plain_password.encode('utf-8'), hashed_password.encode('utf-8'))
+
+# plain_pass = "harsh123"
+# hash_pass = hash_password(plain_pass)
+# print("Harsh password :",hash_pass,type(hash_pass))
+# plain_pass = "harsh123"
+# is_equal = verify_password(plain_pass,hash_pass)
+# print(is_equal)
+
+# raise keyword
+# This keyword is used for generating exception or raising exception or error.
+# Generating error is nothing but creating error object and giving to python
+# runtime system (PVM).
+
+def division(n1,n2):
+    if n1==0 or n2==0:
+        raise ValueError()
+    else:
+        return n1/n2
+
+try:
+    n1 = int(input("Enter num :"))
+    n2 = int(input("Enter num :"))
+    n3 = division(n1,n2)
+    print(n3)
+    print(f"{n1}/{n2} = {n3}")
+except :
+    print("plz.. enter valid integer")
+
+
+# User defined error types or exception types
+# When existing error types does not fulfill application requirement we build
+# our own error types, which are called user defined error types or exception
+# types.
+# Creating error type is nothing creating class.
+
+class ZeroMultiplyError(Exception):
+    pass
+
+def mul(n1,n2):
+    if n1==0 or n2==0:
+        raise ZeroDivisionError()
+    else:
+        return n1*n2
+
+try:
+    n1 = int(input("Enter val :"))
+    n2 = int(input("Enter val :"))
+    n3 = mul(n1,n2)
+    print(f"{n1}*{n2} = {n3}")
+except ValueError:
+    print("Enter valid integer")
+except ZeroDivisionError:
+    print("cannot multiply number by 0")
+
+
+users = {"Harshal":"h123","Aniket":"a123","Kunal":"k123"}
+class LoginError(Exception):
+    def __init__(self, msg=None):
+        super().__init__()
+        self.__msg = msg
+    def __str__(self) -> str:
+        return self.__msg
+def login(unmae,pwd):
+    if unmae in users and users[unmae]==pwd:
+        return f"Welcome {unmae}"
+    else:
+        raise LoginError("Invalid username or password")
+    
+try:
+    name = input("Enter username :")
+    pwd = input("Enter password :")
+    is_login = login(name,pwd)
+    print(is_login)
+except LoginError as lgerror:
+    print(lgerror)
+
+
+class InsuffBalError(Exception):
+    def __init__(self) -> None:
+        super().__init__()
+        self.__msg = "Invalid Balance"
+    
+    def __str__(self) -> str:
+        return self.__msg
+
+class Account:
+    def __init__(self,ano,aname,abalance) -> None:
+        self.__accno = ano
+        self.__accname = aname
+        self.__accbalance = abalance
+    
+    def deposit(self,amount):
+        self.__accbalance += amount
+    
+    def withdraw(self,amount):
+        if amount>self.__accbalance:
+            raise InsuffBalError()
+        else:
+            self.__accbalance-=amount
+    def printinfo(self):
+        print(f"Account no : {self.__accno}\nAccount Holder name : {self.__accname}\nAccount Balance : {self.__accbalance}")
+
+try:
+    a1 = Account(101,"Harshal Kharabe",50000)
+    # a1.deposit(30000)
+    print("Amount deposit successfully!")
+    a1.withdraw(30000)
+    print("Amount withdraw successfully!")
+    a1.printinfo()
+except InsuffBalError as iserror:
+    print(iserror)
+
+# try..except..else block
+# else block is executed when there is no error/exception within try block.
+
+try:
+    n1 = int(input("Enter number :"))
+    n2 = int(input("Enter number :"))
+    n3 = n1/n2
+    print(f'{n1}/{n2} = {n3}')
+except ZeroDivisionError:
+    print("Something went wrong.")
+else:
+    print("I am Else block")
+    
+#try..except..else..finally
+
+try:
+    n1 = int(input("Enter number :"))
+    n2 = int(input("Enter number :"))
+    n3 = n1/n2
+    print(f'{n1}/{n2} = {n3}')
+except ZeroDivisionError:
+    print("Something went wrong.")
+else:
+    print("I am Else block if exception occured in try block that time i am not execute")
+finally:
+    print("I am finally i always execute")
