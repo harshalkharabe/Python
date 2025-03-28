91dcc8f731223b6911c50d099ce810365fedbb2a
Harshkharabe1 <harshal.kharabe@datamatter.tech>
2025-03-28 08:21:34 +0530

    Finding Subarray

--- questions.py
+++ questions.py
@@ -1851,3 +1851,16 @@ def findLongestpalindrome(s):
             longest = palindrome2
 
     return longest
+
+def find_subarray(s1,s2):
+    m, n = len(s1), len(s2)
+    dp = [[0] * (n + 1) for _ in range(m + 1)]
+    max_length = 0  
+
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if s1[i - 1] == s2[j - 1]:  
+                dp[i][j] = dp[i - 1][j - 1] + 1
+                max_length = max(max_length, dp[i][j])
+
+    return max_length
