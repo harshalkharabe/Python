0ed7032e4e3d354b48648261f8602ceca7e4efbd
Harshkharabe1 <harshal.kharabe@datamatter.tech>
2025-05-02 13:49:00 +0530

    MIG

--- questions.py
+++ questions.py
@@ -1908,3 +1908,4 @@ def digital_root(n):
 print(f"Sum of digit : {digital_root(942)}")
 print(f"Sum of digit : {digital_root(94223)}")
 print(f"Sum of digit : {digital_root(9)}")
+print(f"Sum of digit : {digital_root(92)}")
