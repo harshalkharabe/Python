e57c11166420a2c4e066240685c7696ae46b3970
Harshkharabe1 <harshal.kharabe@datamatter.tech>
2025-05-28 09:57:49 +0530

    Two sum

--- questions.py
+++ questions.py
@@ -2080,3 +2080,10 @@ def duplicates(s):
     
     return ''.join(result)
 print(duplicates("aaabbbccddeeffgghhiiijjkkllmmnnooppqqrrssttuuvvwwxxyyzz"))
+
+
+def twoSum(nums, target):
+    for i in range(len(nums)):
+        for j in range(i+1,len(nums)):
+            if nums[i]+nums[j]==target:
+                return [i,j]
