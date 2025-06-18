98febfba7e88a9fa6bda22bf580b5cf701ded46f
Harshkharabe1 <harshal.kharabe@datamatter.tech>
2025-06-18 07:46:25 +0530

    Array in DSA

--- DSA/array.py
+++ DSA/array.py
@@ -36,3 +36,67 @@ arr = [1, 2, 3, 4, 5, 6]
 target = 3
 result = binary_search(arr, target)
 print(f"Element {target} found at index: {result}")  # Output: Element 3 found at index: 2
+
+
+# Find 2 element in sorted array which sum is equal to target
+def find_sum(arr,target):
+    if not arr:
+        return -1
+    arr.sort()
+    lb = 0
+    ub = len(arr) - 1
+    while lb<ub:
+        if arr[lb]+arr[ub]==target:
+            return (arr[lb], arr[ub])
+        elif arr[lb]+arr[ub]<target:
+            lb += 1
+        else:
+            ub -= 1
+    return -1,-1
+
+arr = [20,40,60,80,90,120,240]
+target = 210
+result = find_sum(arr, target)
+print(f"Elements that sum to {target} are: {result}")  # Output: Elements that sum to 7 are: (1, 6) or (2, 5) or (3, 4)
+
+
+# Stock by and sell problem -- Find max profit
+def max_profit(prices):
+    if not prices:
+        return 0
+    min_price = float('inf')
+    max_profit = 0
+    for i in range(len(prices)):
+        if prices[i]<min_price:
+            min_price = prices[i]
+        elif prices[i]-min_price>max_profit:
+            max_profit = prices[i] - min_price
+    return max_profit
+
+prices = [7, 1, 5, 3, 6, 4]
+result = max_profit(prices)
+print(f"Maximum profit is: {result}")  # Output: Maximum profit is: 5
+
+# Find element in 2D array
+arr = [[1,4,7],[9,12,45],[112,114,213]]
+
+def find_element_2d(arr, target):
+    if not arr or not arr[0]:
+        return -1, -1
+    rows = len(arr)
+    cols = len(arr[0])
+    right,left = rows*cols-1,0
+    while left<right:
+        mid = (left + right) // 2
+        mid_ele = arr[mid//cols][mid%cols]
+        if mid_ele == target:
+            return mid // cols, mid % cols
+        elif mid_ele < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1, -1
+
+target = 12
+result = find_element_2d(arr, target)
+print(f"Element {target} found at position: {result}")  # Output: Element 12 found at position: (1, 1)
