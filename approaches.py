6624674b71c279c5f458558e9ccfee9ead2d9dc2
Harshkharabe1 <harshal.kharabe@datamatter.tech>
2025-06-30 13:17:35 +0530

    DSA

--- approaches.py
+++ approaches.py
@@ -100,4 +100,5 @@ def move_zeros(arr):
             left += 1
     return arr
 arr = [0, 1, 0, 3, 12]
+arr = [0, 1, 0, 3, 12]
 print(f"Array after moving zeros to end: {move_zeros(arr)}")  # Output: [1, 3, 12, 0, 0]
